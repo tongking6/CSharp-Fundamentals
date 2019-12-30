2ba8e687608eee0d92ae35adf1967dfd7ab42edf
Tong Jin <tongjin1992@gmail.com>
2019-12-29 21:38:42 -0500

    2. Learn the C# Syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,13 +7,22 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.7 };
+            var result = 0.0;
+            foreach(double number in grades) 
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is: {result:N1}");
+
             if(args.Length > 0) 
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
             else 
             {
-                Console.WriteLine("Hello!");
+                   Console.WriteLine("Hello!");
             }
         }
     }
